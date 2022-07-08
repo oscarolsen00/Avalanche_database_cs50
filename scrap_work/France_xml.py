bf69590e24771d83c391986fefd9c65389424683
oscarolsen00 <74254115+oscarolsen00@users.noreply.github.com>
2022-07-08 17:01:37 +0200

    additonal parts extracted from html

--- scrap_work/France_xml.py
+++ scrap_work/France_xml.py
@@ -2,7 +2,8 @@ from bs4 import BeautifulSoup
 import lxml
 
 
-file = open('MB.xml', 'r')
+
+file = open('MB2.xml', 'r')
 contents = file.read()
 
 soup = BeautifulSoup(contents, 'lxml')
@@ -25,6 +26,32 @@ if west in rosette_string:
 else:
     print("not found")
 
+for link in soup.find_all('bulletins_neige_avalanche'):
+    print(link.get('massif'))
+    print(link.get('datebulletin'))
+   
+
+for link2 in soup.find_all('risque'):
+    print(link2.get('risque1'))
+    print(link2.get('altitude'))
+    print(link2.get('risque2'))
+
+# print(soup.find_all('risque'))
+
+# # right now the for loop in the function is run multiple times need to figure out where the other risques are coming from
+# def risk_fun():
+#     for link2 in soup.find_all('risque'):
+#         risk1 = str(link2.get('risque1'))
+#         altitude = str(link2.get('altitude'))
+#         risk2 = str(link2.get('risque2'))
+#     return risk1, risk2, altitude
+
+
+
+#want to get date
+#want to get elevation
+#want to get risk level
+#location
 
 
 

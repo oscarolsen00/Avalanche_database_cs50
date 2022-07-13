6feda222da6c7462af279edbd3d2f70f78478e11
oscarolsen00 <74254115+oscarolsen00@users.noreply.github.com>
2022-07-13 11:56:38 +0200

    scrap work on swiss

--- scrap_work/swiss_scapper.py
+++ scrap_work/swiss_scapper.py
@@ -9,22 +9,22 @@ import pandas as pd
 
 ###################  extracts relevant text ###################
 
-# importing required modules 
-import PyPDF2 
+# # importing required modules 
+# import PyPDF2 
  
-# creating a pdf file object 
-pdfFileObj = open('swiss_ab.pdf', 'rb') 
+# # creating a pdf file object 
+# pdfFileObj = open('swiss_ab.pdf', 'rb') 
  
-# creating a pdf reader object 
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-# creating a page object 
-pageObj = pdfReader.getPage(0) 
+# # creating a pdf reader object 
+# pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+# # creating a page object 
+# pageObj = pdfReader.getPage(0) 
  
-# extracting text from page 
-print(pageObj.extractText()) 
+# # extracting text from page 
+# print(pageObj.extractText()) 
  
-# closing the pdf file object 
-pdfFileObj.close()
+# # closing the pdf file object 
+# pdfFileObj.close()
 
 
 
@@ -49,4 +49,36 @@ pdfFileObj.close()
 #                 image = doc.extract_image(xref)
 #                 pix = fitz.Pixmap(doc, xref)
 #                 pix.save(os.path.join(workdir, "%s_p%s-%s.png" % (each_path[:-4], i, xref)))
+#
+#
+# looking for 
+#
+##importing the necessary libraries
+import fitz
+
+#opeing the file
+file_path = input("swiss_ab.pdf")
+pdf_file = fitz.open(file_path)
+
+#Reading the location where to save the file
+location = input("/Users/oscarolsen/Documents/FATMAP/Round_2/Avalanche_database_cs50/scrap_work")
+
+#finding number of pages in the pdf
+number_of_pages = len(pdf_file)
+
+#iterating through each page in the pdf
+for current_page_index in range(number_of_pages):
+  #iterating through each image in every page of PDF
+  for img_index,img in enumerate(pdf_file.getPageImageList(current_page_index)):
+        xref = img[0]
+        image = fitz.Pixmap(pdf_file, xref)
+        #if it is a is GRAY or RGB image
+        if image.n < 5:        
+            image.writePNG("{}/image{}-{}.png".format(location,current_page_index, img_index))
+        #if it is CMYK: convert to RGB first
+        else:                
+            new_image = fitz.Pixmap(fitz.csRGB, image)
+            new_image.writePNG("{}/image{}-{}.png".foramt(location,current_page_index, img_index))
+
+
                
